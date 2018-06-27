7dfd39d3165018acb8e4a97f07fefc228454fc7f
khuhtinen <kukkahu@gmail.com>
2018-06-27 14:17:49 +0300

    Implemented the creation of the array

--- programming/c/datastructures-functions-heat/2d_array.c
+++ programming/c/datastructures-functions-heat/2d_array.c
@@ -9,7 +9,27 @@ int main(void)
     int i, j;
     // Add here the definition for array with name 'array'
     // of size NX times NY (258x258)
-#error Define the array
+    float array[NX][NY];
+
+    for(i=0;i<NX;i++) {
+      for(j=0;j<NY;j++) {
+	if(i==0) {
+	  array[i][j] = 85.0; //Upper boundary
+	}
+	else if(i==NX-1) {
+	  array[i][j] = 5.0; //Lower boundary
+	}
+	else if(j==0) {
+	  array[i][j] = 20.0; //Left boundary
+	}
+	else if(j==NY-1) {
+	  array[i][j] = 70.0; //Right boundary
+	}
+	else {
+	  array[i][j] = 0.0;
+	}
+      }
+    }
 
     // Initialize first the zeros
 #error Initialize the zeros for interior
