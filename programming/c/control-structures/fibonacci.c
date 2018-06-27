04548496d20dbad80e8eda57d0736bd23fabaaf7
khuhtinen <kukkahu@gmail.com>
2018-06-27 11:37:04 +0300

    Fibonacci numbers exercise

--- programming/c/control-structures/fibonacci.c
+++ programming/c/control-structures/fibonacci.c
@@ -2,10 +2,24 @@
 
 int main(int argc, char *argv[])
 {
-    int f0, f1, f2;
+  long unsigned int f0, f1, f2;
+  int n = 2;
 
     // initialize variables and write the while loop
     // Remember to update all variables within the loop
+    f0 = 0;
+    f1 = 1;
+    f2 = f0 + f1;
+
+    printf("\t0\t%lu\n\t1\t%lu\n",f0,f1);
+
+    while(n < 100) {
+      n++;
+      printf("\t%d\t%lu\n",n,f2);
+      f0 = f1;
+      f1 = f2;
+      f2 = f0 + f1;
+    }
 
     return 0;
 }
