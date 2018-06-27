599f1b99e3f673f55d008aa641f5a5314fd5af5c
khuhtinen <kukkahu@gmail.com>
2018-06-27 19:18:05 +0300

    Implemented complex addition

--- programming/c/mandelbrot/mandelbrot.c
+++ programming/c/mandelbrot/mandelbrot.c
@@ -7,13 +7,21 @@ typedef struct {
 } complex; 
 
 
-complex *square(complex * z) {
+void *square(complex * z) {
   
   complex * res;
   res->real = z->real*z->real - z->imag*z->imag;
   res->imag = 2*z->real*z->imag;
 
-  return res;
+  z->real = res->real;
+  z->imag = res->imag;
+}
+
+void *add(complex * z, complex * c) {
+  
+  z->real += c->real;
+  z->imag += c->imag;
+
 }
 
 int main() {
