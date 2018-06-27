b7e206e4d6a079c84eaabd6d609b605c4e354102
khuhtinen <kukkahu@gmail.com>
2018-06-27 10:34:04 +0300

    Expressions exercise.

--- programming/c/datatypes-expressions/expressions.c
+++ programming/c/datatypes-expressions/expressions.c
@@ -2,12 +2,18 @@
 
 int main(int argc, char *argv[])
 {
-    // declare integers, floats, and doubles
+  // declare integers, floats, and doubles
+  int i = 5;
+  double d = 3.0;
+  float f = 5.5;
 
+  // evaluate expressions, e.g. c = a + 2.5 * b
+  // and print out results. Try to combine integers, floats
+  // and doubles and investigate what happens in type conversions
 
-    // evaluate expressions, e.g. c = a + 2.5 * b
-    // and print out results. Try to combine integers, floats
-    // and doubles and investigate what happens in type conversions
+  f++;
+
+  printf("The result is %f.\n",f);
 
     return 0;
 }
