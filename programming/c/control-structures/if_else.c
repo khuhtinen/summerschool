a0a9f6aebca180cc4a5a03727d6522f5b1655df3
khuhtinen <kukkahu@gmail.com>
2018-06-27 11:21:02 +0300

    Added control structure

--- programming/c/control-structures/if_else.c
+++ programming/c/control-structures/if_else.c
@@ -2,10 +2,20 @@
 
 int main(int argc, char *argv[])
 {
-    int i;
+    int i = -1;
 
     // write the control structure as described in the exercise
     // and run the code with different values for i
+
+    if(i < 0) {
+      printf("Integer is negative.\n");
+    }
+    else if(i == 0) {
+      printf("Integer is zero.\n");
+    }
+    else if(i>100) {
+      printf("Integer is larger than 100.\n");
+    }
 
     return 0;
 }
