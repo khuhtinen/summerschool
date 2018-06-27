8813cf1a39f46cd69c897616b246a82ae5afd206
khuhtinen <kukkahu@gmail.com>
2018-06-27 19:21:30 +0300

    Implemented iteration of polynomial

--- programming/c/mandelbrot/mandelbrot.c
+++ programming/c/mandelbrot/mandelbrot.c
@@ -7,7 +7,7 @@ typedef struct {
 } complex; 
 
 
-void *square(complex * z) {
+void square(complex * z) {
   
   complex * res;
   res->real = z->real*z->real - z->imag*z->imag;
@@ -17,12 +17,32 @@ void *square(complex * z) {
   z->imag = res->imag;
 }
 
-void *add(complex * z, complex * c) {
+void add(complex * z, complex * c) {
   
   z->real += c->real;
   z->imag += c->imag;
 
 }
+
+/*
+  Computes the N:th iterate of 0 for the polynomial z^2+c.
+ */
+void iterate(complex *c, int N) {
+
+  int i;
+  complex *z;
+  z->real = 0.0;
+  z->imag = 0.0;
+
+  for(i=0; i<N; i++) {
+    square(z);
+    add(z,c);
+  }
+
+}
+
+
+
 
 int main() {
   
