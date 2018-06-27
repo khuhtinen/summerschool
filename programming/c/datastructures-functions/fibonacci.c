eb545c15cf7de5d45db50edb2f268c83967400fc
khuhtinen <kukkahu@gmail.com>
2018-06-27 13:51:00 +0300

    Fibonacci exercise

--- programming/c/datastructures-functions/fibonacci.c
+++ programming/c/datastructures-functions/fibonacci.c
@@ -7,6 +7,11 @@ int main(int argc, char *argv[])
     int i;
 
     // initialize necessary array elements and write the for loop
+    f[0] = 0;
+    f[1] = 1;
+    for (i = 2; i<20; i++) {
+      f[i] = f[i-1] + f[i-2];
+    }
 
     printf("First 20 Fibonacci numbers are:\n");
     for (i = 0; i < 20; i++) {
