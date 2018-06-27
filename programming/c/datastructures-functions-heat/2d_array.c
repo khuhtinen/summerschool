b74593dca5543bb3e490ee2ce65e455caa22eb86
khuhtinen <kukkahu@gmail.com>
2018-06-27 14:30:38 +0300

    Modified the implementation

--- programming/c/datastructures-functions-heat/2d_array.c
+++ programming/c/datastructures-functions-heat/2d_array.c
@@ -11,34 +11,24 @@ int main(void)
     // of size NX times NY (258x258)
     float array[NX][NY];
 
-    for(i=0;i<NX;i++) {
-      for(j=0;j<NY;j++) {
-	if(i==0) {
-	  array[i][j] = 85.0; //Upper boundary
-	}
-	else if(i==NX-1) {
-	  array[i][j] = 5.0; //Lower boundary
-	}
-	else if(j==0) {
-	  array[i][j] = 20.0; //Left boundary
-	}
-	else if(j==NY-1) {
-	  array[i][j] = 70.0; //Right boundary
-	}
-	else {
-	  array[i][j] = 0.0;
-	}
+    // Initialize first the zeros
+    for(i = 0; i<NX; i++) {
+      for(j = 0; j<NY; j++) {
+	array[i][j] = 0;
       }
     }
 
-    // Initialize first the zeros
-#error Initialize the zeros for interior
-
     // Initial conditions for left and right
-#error add boundary conditions for left and right
+    for(i = 1; i<NX-1; i++) {
+      array[i][0] = 20.0; //Left
+      array[i][NY-1] = 70.0; //Right
+    }
 
     // and top and bottom boundaries
-#error add boundary conditions for top and bottom
+    for(i = 1; i<NY-1; i++) {
+      array[0][i] = 85.0; //Upper
+      array[NX-1][i] = 5.0; //Lower
+    }
 
     return 0;
 }
