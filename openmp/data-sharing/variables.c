d918590b69865b7f2eeb8cdc3e86ad42cd09a880
khuhtinen <kukkahu@gmail.com>
2018-06-29 10:08:17 +0300

    Added OpenMP parallel regions

--- openmp/data-sharing/variables.c
+++ openmp/data-sharing/variables.c
@@ -2,17 +2,32 @@
 
 int main(void)
 {
-    int var1 = 1, var2 = 2;
+  int var1 = 1, var2 = 2;
 
-    /* TODO:
-     *   Test the effect of different data sharing clauses here
-     */
-    {
-        printf("Region 1: var1=%i, var2=%i\n", var1, var2);
-        var1++;
-        var2++;
-    }
-    printf("After region 1: var1=%i, var2=%i\n\n", var1, var2);
+  #pragma omp parallel private(var1,var2)
+  {
+    printf("Region 1: var1=%i, var2=%i\n", var1, var2);
+    var1++;
+    var2++;
+  }
+  printf("After region 1: var1=%i, var2=%i\n\n", var1, var2);
+  
+  #pragma omp parallel firstprivate(var1,var2)
+  {
+    printf("Region 2: var1=%i, var2=%i\n", var1, var2);
+    var1++;
+    var2++;
+  }
+  printf("After region 2: var1=%i, var2=%i\n\n", var1, var2);
 
-    return 0;
+  #pragma omp parallel shared(var1,var2)
+  {
+    printf("Region 2: var1=%i, var2=%i\n", var1, var2);
+    var1++;
+    var2++;
+  }
+  printf("After region 2: var1=%i, var2=%i\n\n", var1, var2);
+
+  
+  return 0;
 }
