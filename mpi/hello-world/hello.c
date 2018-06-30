6492f058b1b638704510935b088291ee38536b32
khuhtinen <kukkahu@gmail.com>
2018-06-30 14:18:43 +0300

    Modified according to the exercise

--- mpi/hello-world/hello.c
+++ mpi/hello-world/hello.c
@@ -3,20 +3,25 @@
 
 int main(int argc, char *argv[]) {
 
-  int N,n;
+  int n;
 
   MPI_Init(&argc,&argv);
   
-  MPI_Comm_size(MPI_COMM_WORLD,&N);
   MPI_Comm_rank(MPI_COMM_WORLD,&n);
   
   printf("Hello world! ");
 
   if(n%2==0)
-    printf("I'm thread %d/%d.\n",n,N);
+    printf("I'm thread %d.\n",n);
   else
     printf("I'm %d. I'm odd.\n",n);
 
+  if(n==0) {
+    int N;
+    MPI_Comm_size(MPI_COMM_WORLD,&N);
+    printf("There is a total of %d of us.\n",N);
+  }
+  
   MPI_Finalize();
   
 
