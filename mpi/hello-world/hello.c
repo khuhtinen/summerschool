ac90510a8c72a379dfee4d6ccb53d87ca372e8ef
khuhtinen <kukkahu@gmail.com>
2018-06-30 14:06:34 +0300

    Hello world with MPI

---
+++ mpi/hello-world/hello.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+
+  int N,n;
+
+  MPI_Init(&argc,&argv);
+  
+  MPI_Comm_size(MPI_COMM_WORLD,&N);
+  MPI_Comm_rank(MPI_COMM_WORLD,&n);
+
+  if(n%2==0)
+    printf("Hello world, from thread %d/%d\n",n,N);
+  else
+    printf("I'm %d. I'm odd.\n",n);
+
+  MPI_Finalize();
+  
+
+  return 0;
+
+}
