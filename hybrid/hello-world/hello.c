28f1fa1caa34251972abc927845bbda4bf5c7766
khuhtinen <kukkahu@gmail.com>
2018-07-02 15:24:36 +0300

    Removed the TODO comments

--- hybrid/hello-world/hello.c
+++ hybrid/hello-world/hello.c
@@ -8,11 +8,8 @@ int main(int argc, char *argv[])
     int my_id, omp_rank;
     int provided, required = MPI_THREAD_FUNNELED;
 
-    /* TODO: Initialize MPI with thread support. */
     MPI_Init_thread(&argc,&argv,required,&provided);
     
-    /* TODO: Find out the MPI rank and thread ID of each thread and print
-     *       out the results. */
     MPI_Comm_rank(MPI_COMM_WORLD,&my_id);
 
 #pragma omp parallel private(omp_rank)
