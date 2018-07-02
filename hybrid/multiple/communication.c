784751c4b9316580a81d980616f85ed216c4bc7f
khuhtinen <kukkahu@gmail.com>
2018-07-02 19:02:39 +0300

    Added sends and receives

--- hybrid/multiple/communication.c
+++ hybrid/multiple/communication.c
@@ -12,28 +12,31 @@ int main(int argc, char* argv[]) {
   int i;
   
   MPI_Init_thread(&argc,&argv,required,&provided);
-  MPI_Comm_rank(MPI_COMM_WORLD,&my_id);
 
+  if(provided < MPI_THREAD_MULTIPLE) {
+    printf("MPI_THREAD_MULTIPLE not supported.\n");
+    MPI_Abort(MPI_COMM_WORLD, -1);
+
+  }
+  
+  MPI_Comm_rank(MPI_COMM_WORLD,&my_id);
   MPI_Comm_size(MPI_COMM_WORLD,&np);
   
-#pragma omp parallel private(omp_rank, msg, i)
+#pragma omp parallel private(omp_rank, nt, msg, i)
   {
     //Get my thread number and the total number of threads
     omp_rank = omp_get_thread_num();
     nt = omp_get_num_threads();
     
-    //Initialize my int to be sent or received
-    if(my_id==0) {
-      msg = omp_rank;
+    if(my_id == 0) {
+      msg = omp_rank + 10;
+      for(i=1;i<np;i++) {
+	MPI_Send(&omp_rank,1,MPI_INT,i,omp_rank,MPI_COMM_WORLD);
+      }
     }
-    else{
-      msg = -1;
+    else {
+      MPI_Recv(&msg,1,MPI_INT,0,omp_rank,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
     }
-
-    // Initial print
-    printf("My initial: process %d thread %d msg %d \n",my_id,omp_rank,msg);
-
-    
     
     // Print result
     printf("My final: process %d thread %d msg %d \n",my_id,omp_rank,msg);
