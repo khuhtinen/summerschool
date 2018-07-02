a27f1ea28872a4e02fd7e59ee67a32afd908b83b
khuhtinen <kukkahu@gmail.com>
2018-07-02 11:23:59 +0300

    Datatype for particle

--- mpi/datatypes/c/struct_type.c
+++ mpi/datatypes/c/struct_type.c
@@ -6,18 +6,20 @@
 
 int main(int argc, char *argv[])
 {
-    int n = 1000, cnt = 3, reps = 10000;
-    particle particles[n];
-    int i, j, myid, ntasks, blocklen[cnt];
-    MPI_Datatype particletype, temptype;
-    MPI_Aint disp[cnt], dist[2], lb, extent;
-    double t1, t2;
-
+  
     typedef struct {
         float coords[3];
         int charge;
         char label[2];
     } particle;
+
+
+    int n = 1000, cnt = 3, reps = 10000;
+    particle particles[n];
+    int i, j, myid, ntasks;
+    MPI_Datatype particletype,temptype;
+    MPI_Aint disp[cnt], dist[2], lb, extent;
+    double t1, t2;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
@@ -32,18 +34,28 @@ int main(int argc, char *argv[])
             strcpy(particles[i].label, "Xe");
         }
     }
-    /* TODO (c): define the datatype for the struct particle  using MPI_Type_create_struct
-       You can use MPI_Get_address to compute offsets.
-    */
 
-    /* TODO (c): check extent (not really necessary on most platforms) That is,
-     * check that extent is identical to the distance between two consequtive
-     * structs in an array
-     * Tip, use MPI_Type_get_extent and  MPI_Get_address
-     */
+    MPI_Get_address(&particles[0].coords,&disp[0]);
+    MPI_Get_address(&particles[0].charge,&disp[1]);
+    MPI_Get_address(&particles[0].label,&disp[2]);
+    disp[2] = disp[2]-disp[0];
+    disp[1] = disp[1]-disp[0];
+    disp[0] = 0;
+    int blocklen[3] = {3,1,2};
+    MPI_Datatype types[3] = {MPI_FLOAT,MPI_INT,MPI_CHAR};
 
+    MPI_Type_create_struct(3,blocklen,disp,types,&particletype);
+    MPI_Type_commit(&particletype);
+
+    
+    MPI_Type_get_extent(particletype,&lb,&extent);
+    MPI_Get_address(&particles[0],&dist[0]);
+    MPI_Get_address(&particles[1],&dist[1]);
+    
     if (extent != (dist[1] - dist[0])) {
-        /*TODO (c), resize particle type to correct extent */
+      temptype = particletype;
+      MPI_Type_create_resized(temptype,0,dist[1]-dist[0],&particletype);
+      MPI_Type_free(&temptype);
     }
 
     /* communicate using the created particletype */
@@ -65,7 +77,7 @@ int main(int argc, char *argv[])
            particles[n - 1].coords[0], particles[n - 1].coords[1],
            particles[n - 1].coords[2]);
 
-    //TODO: Free datatype
+    MPI_Type_free(&particletype);
 
     MPI_Finalize();
     return 0;
